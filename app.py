ad03caf87390990848b992e940f0281e30932e41
Kumar Saksham <everydaycodings@gmail.com>
2021-12-06 17:43:58 +0530

    encounting an error with api_fetching() giving wrong id from anime.csv in helper.py

--- app.py
+++ app.py
@@ -8,7 +8,19 @@ st.title("Anime Recommender System")
 selected_anime_name = st.selectbox("Enter The Name of the Anime", options = helper.fetch_data_for_options())
 
 if st.button("Recommend"):
-    movie_name, recommend_list = helper.recommend(selected_anime_name)
+    movie_name, recommend_list, recommend_poster_list = helper.recommend(selected_anime_name)
     st.write(movie_name)
-    for i in recommend_list:
-        st.write(i)
+    
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.header("A cat")
+        st.image("https://static.streamlit.io/examples/cat.jpg")
+
+    with col2:
+        st.header("A dog")
+        st.image("https://static.streamlit.io/examples/dog.jpg")
+
+    with col3:
+        st.header("An owl")
+        st.image("https://static.streamlit.io/examples/owl.jpg")
